f8571da8450f7b457c99b22f39d9793757fc9435
mattwehner <mattwehnernz@gmail.com>
2019-08-14 16:32:10 -0600

    Confetti Cannons

---
+++ Assets/Scripts/ConfettiBehavior.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ConfettiBehavior : MonoBehaviour
+    {
+        private Transform _confetti;
+        private Rect _screenBounds;
+        private bool _startedInBounds;
+
+        void Awake()
+        {
+            _confetti = gameObject.transform;
+            _screenBounds = new Rect(0f, 0f, Screen.width, Screen.height);
+
+            //Set random fall speed
+            float drag = _confetti.GetComponent<Rigidbody2D>().drag;
+            float randomRange = 0.08f;
+            float randomMass = Random.Range(drag - randomRange, drag + randomRange);
+            _confetti.GetComponent<Rigidbody2D>().drag = randomMass;
+        }
+
+        void Start()
+        {
+            Vector2 confettiPosition = _confetti.position;
+            _startedInBounds = _screenBounds.Contains(confettiPosition);
+        }
+
+        void Update ()
+        {
+            //TODO: Rotate to face down over time
+
+            //Destroy if off screen
+            Vector2 confettiPosition = _confetti.position;
+            if (!_screenBounds.Contains(confettiPosition))
+            {
+                Destroy(_confetti.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ConfettiLauncher.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class ConfettiLauncher : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _confetti;
+
+        [Header("Launcher Settings")]
+        [SerializeField]
+        private int _confettiAmount = 30;
+        [SerializeField]
+        private float _launchForce = 10000f;
+        [SerializeField]
+        private float _randomizeForce = 30f;
+        [SerializeField]
+        private float _spreadAmount = 100f;
+
+
+        private List<GameObject> _confettiCollection;
+
+        void Awake()
+        {
+            SpawnConfetti();
+            LaunchConfetti();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                SpawnConfetti();
+                LaunchConfetti();
+            }
+        }
+
+        private void LaunchConfetti()
+        {
+            foreach (var confetti in _confettiCollection)
+            {
+                float randomForce = Random.Range(_launchForce - _randomizeForce, _launchForce + _randomizeForce);
+                confetti.GetComponent<Rigidbody2D>().AddRelativeForce(confetti.transform.up * randomForce, ForceMode2D.Impulse);
+            }
+        }
+
+        private void SpawnConfetti()
+        {
+            _confettiCollection = new List<GameObject>();
+
+            for (int i = 0; i < _confettiAmount; i++)
+            {
+                GameObject confetti = Instantiate(_confetti, transform.position, Quaternion.identity, transform);
+                _confettiCollection.Add(confetti);
+
+                //Assign Random Color
+                confetti.GetComponent<Image>().color = Random.ColorHSV(0,1,0.6f,1,1,1);
+
+                //Rotate confetti randomly for spread
+                float zRotation = confetti.transform.rotation.z;
+                float rotateAmount = Random.Range(zRotation - _spreadAmount, zRotation + _spreadAmount);
+                confetti.transform.Rotate(0,0, rotateAmount);
+            }
+        }
+    }
+}
